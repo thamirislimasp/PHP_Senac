b1d1bef6d12ba0d197d2bac896f7be3ae8f0d341
thamirislimasp <thamirislimasp@gmail.com>
2024-06-04 21:59:36 -0300

    Conta Corrente e Conta Poupança

--- 00/script.js
+++ 00/script.js
@@ -23,5 +23,45 @@ class Conta {
     }
 }
 
+class ContaCorrente extends Conta {
+    constructor(numero, saldo = 0, limiteChequeEspecial = 200) {
+        super(numero, saldo);
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    sacar(valor) {
+        //super.sacar(valor);
+        const valorDisponivelSaque = this.saldo + this.limiteChequeEspecial;
+
+        if(valor > 0 && valorDisponivelSaque >= valor) {
+            this.saldo -= valor;
+        } else {
+            console.log("Saldo insuficiente");
+        }
+    }
+}
+
+class ContaPoupanca extends Conta{
+    // crie o construtor da conta poupança, com uma propriedade taxaRendimento
+    // a taxa é por parão 0.2
+    // crie uma função aplicarRendimento() que não precisa fazer nada por hora
+    constructor(numero, saldo = 0, taxaRendimento = 0.2) {
+        super(numero, saldo);
+        this.taxaRendimento = taxaRendimento;
+    }
+
+    aplicarRendimento() {
+        console.log("Rendimento Aplicado");
+    }
+}
+
+let contaC = new ContaCorrente(2001, 500);
+let contaP = new ContaCorrente(2002, 500);
+
+console.log(contaC.toString());
+contaC.sacar(650);
+console.log(contaC.toString());
 
 
+contaP.aplicarRendimento();
+//contaC.aplicarRendimento(); # erro por não existir função nessa classe
