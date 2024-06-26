07592e6ea0abaddb95e967680bdfb3dbe61e0371
Thamiris <thamirislimasp@gmail.com>
2024-06-25 21:32:27 -0300

    Resoluções Atividades

--- 05/exercicios.php
+++ 05/exercicios.php
@@ -165,87 +165,138 @@
 /*
     Solicita ao usuário um número inteiro e calcula o quadrado desse número.
 */
-$numero = 8;
+    $numero = 8;
+
 // Implementação do Cálculo do Quadrado
 
+    $quadrado = $numero * $numero;
+
+    echo "O quadrado de $numero é $quadrado <br>";
+
+    echo "<br>";
+    echo "<hr>";
 
 // Atividade 3: Verificar se um número é par ou ímpar
 /*
     Solicita ao usuário um número inteiro e verifica se ele é par ou ímpar.
 */
-$numero = 14;
+    $numero = 14;
 // Implementação da Verificação de Paridade
+    if($numero % 2 == 0) {
+        echo "$numero é um número par <br>";
+    } else {
+        echo "$numero é um número ímpar <br>";
+    }
 
+    echo "<br>";
+    echo "<hr>";
 
 // Atividade 4: Calcular a média de três números
 /*
     Solicita ao usuário três números e calcula a média aritmética entre eles.
 */
-$numero1 = 10;
-$numero2 = 15;
-$numero3 = 20;
+    $numero1 = 10;
+    $numero2 = 15;
+    $numero3 = 20;
 
 // Implementação do Cálculo da Média
+    $media = ($numero1 + $numero2 + $numero3) / 3;
+    echo "A média entre $numero1 + $numero2 + $numero3 é: $media <br>";
 
-
+    echo "<br>";
+    echo "<hr>";
 
 // Atividade 5: Verificar se um número é múltiplo de outro
 /*
     Solicita ao usuário dois números e verifica se o primeiro é múltiplo do segundo.
 */
-$numeroMultiplo = 15;
-$multiploDe = 5;
+    $numeroMultiplo = 15;
+    $multiploDe = 5;
 // Implementação da Verificação de Múltiplo
+    if($numeroMultiplo % $multiploDe == 0) {
+        echo "$numeroMultiplo é múltiplo de $multiploDe <br>";
+    } else {
+        echo "$numeroMultiplo não é múltiplo de $multiploDe <br>";
+    }
+
+    echo "<br>";
+    echo "<hr>";
 
 //// Atividade 6: Conversão de Horas em Minutos
 /*
     Solicita ao usuário a quantidade de horas e minutos e converte isso para o equivalente total de minutos.
 */
-$horas_string = "01:30";
+    $horas_string = "01:30";
 
 // Implementação de Conversão de Horas em Minutos
 
-echo "O equivalente total em minutos de $horas_string é: XX <br>";
+    $horas = explode(":", $horas_string);
+    $total_minutos = $horas[0] * 60 + $horas[1];
 
+    echo "O equivalente total em minutos de $horas_string é: $total_minutos <br>";
 
+    echo "<br>";
+    echo "<hr>";
 
 /// Atividade 7: Desenho de Pirâmide
 /*
     Solicita ao usuário a altura de uma pirâmide (número de linhas) e desenha-a na tela usando asteriscos (*) em cada linha.
 */
-$altura_piramide = 5;
+    $altura_piramide = 5;
+
 // Implementação de Desenho de Pirâmide
+    for($i = 1; $i <= $altura_piramide; $i++) {
+        for($j = 1; $j <= $i; $j++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
 
-
+    echo "<br>";
+    echo "<hr>";
 
 //// Atividade 8: Contagem de Dígitos em um Número
 /*
     Solicita ao usuário um número inteiro positivo e conta quantos dígitos esse número possui.
 */
-$numeroDigitos = 2048;
+    $numeroDigitos = 2048;
 // Implementação de Contagem de Dígitos em um Número
+    
+    echo "A quantidade de dígitos do número $numeroDigitos é : " . strlen($numeroDigitos);
 
-
+    echo "<br>";
+    echo "<hr>";
 
 //// Atividade 9: Verificação de Palíndromo
 /*
     Solicita ao usuário uma palavra e verifica se ela é um palíndromo.
 */
-$palavra = "abbabba";
+    $palavra = "abbabba";
 
 // Implementação de Verificação de Palíndromo
 
+    $contrario_dela = strrev($palavra);
+
+    if($palavra == $contrario_dela) {
+        echo "A plavra $palavra é um palíndromo <br>";
+    } else {
+        echo "A plavra $palavra não é um palíndromo <br>";
+    }    
+
+    echo "<br>";
+    echo "<hr>";
 
 
 //// Atividade 10: Ordenação de Números
 /*
     Solicita ao usuário três números inteiros e os exibe em ordem crescente.
 */
-$numero1 = 25;
-$numero2 = 1;
-$numero3 = 7;
+    $numero1 = 25;
+    $numero2 = 1;
+    $numero3 = 7;
 
 // Implementação de Ordenação de Números
+
 
 
 
@@ -253,7 +304,13 @@ $numero3 = 7;
 /*
     Solicita ao usuário um número inteiro positivo e calcula a soma de todos os seus dígitos.
 */
-$numeroSoma = 599;
+    $numeroSoma = 599;
+
 // Implementação de Soma de Dígitos
+
+    $numeros_array = str_split($numeroSoma);
+    $somaDigitos = array_sum($numeros_array);
+
+    echo "A soma dos dígitos de $numeroSoma é: $somaDigitos";
 
 ?>
