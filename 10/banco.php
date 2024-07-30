b77ef5ac175a143ac931962ffaa31c7712a2692f
Thamiris <thamirislimasp@gmail.com>
2024-07-30 20:30:48 -0300

    funções

---
+++ 10/banco.php
@@ -0,0 +1,28 @@
+<?php
+
+//Função para conectar ao banco de dados
+function connect() {
+
+}
+
+//Função para obter todos os itens (Read)
+function getAllItems() {
+
+}
+
+//Função para adicionar um novo item (Create)
+function addItem($nome_produto, $quantidade) {
+
+}
+
+//Função para atualizar um item (Update)
+function updateItem($id, $comprado) {
+
+}
+
+//Função para excluir um item (Delete)
+function deleteItem($id) {
+
+}
+
+?>
